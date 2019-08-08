b17b9b0cd94ac46a52725b9c8d1dccb8e845d114
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-08-07 23:06:07 -0500

    Almost finished the nametags. Found it was easier to add just one class name and then style it from the main.css file instead of having long div and styles. Cannot get it to print out each name individually it is still only printing just the first name. I need to find the correct selector for that.

--- renderNametags.js
+++ renderNametags.js
@@ -1,12 +1,31 @@
 
 function renderNametags(nametags) {
-    return `
-        <div class="text-center mt-5">
-					<div style="
-					name: ${nametags}
-					">
-        </div>
-    `
+    // return `    original code to put names on a tag...
+    //     <div class="text-center mt-5">
+					
+		// 		<div style="
+		// 			color: white;
+		// 			background-color: blue;
+		// 			">Hello, my name is:</div>
+		// 			<div style="
+		// 			text-align: center">
+		// 			${nametags}
+		// 			</div>
+					
+    //     </div>
+		// `
+		
+		function createNameTag (name) {
+			for (var i = 0; i < name.length; i++){
+			return `<div class="text-center mt-5">
+							<div class="nameTagTop">Hello, my name is:</div>
+							<div class="nameTagBottom">${nametags[i]}</div>
+							</div>`
+						}
+		}
+		let newNameTag = nametags.map(createNameTag)
+
+		return newNameTag.join("")
 }
 
 function nametags() {
